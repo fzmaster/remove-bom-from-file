0da11ce0f2133c32f0f4370613adbcb2236e99c0
Fernando Zanardo <fzmaster@gmail.com>
2018-09-28 19:07:45 -0300

    Added comments to Original file.

--- processBOM.py
+++ processBOM.py
@@ -1,7 +1,10 @@
+
+# Open file with list of BOM-ed files
 with open(r'C:\testes\output\Lista-Arquivos-Problemas.txt') as f:
     lines = f.read().splitlines()
 #print(lines.__len__())
 
+# Loop each file solving the problem - It will overwrite the original file
 for file in lines:
     s = open(file, mode='r', encoding='utf-8-sig').read()
     open(file, mode='w', encoding='utf-8').write(s)
